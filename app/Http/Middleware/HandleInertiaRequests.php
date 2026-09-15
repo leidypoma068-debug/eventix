6a48945fd6f2763d1dc3ccaa1dfb1209fda2e557
Leidy Estefania Poma Casas <leidypoma068@gmail.com>
2026-09-14 20:29:19 -0400

    chore: incorporar núcleo HTTP y middleware

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+use Tightenco\Ziggy\Ziggy;
+
+class HandleInertiaRequests extends Middleware
+{
+    /**
+     * The root template that is loaded on the first page visit.
+     *
+     * @var string
+     */
+    protected $rootView = 'app';
+
+    /**
+     * Determine the current asset version.
+     */
+    public function version(Request $request): string|null
+    {
+        return parent::version($request);
+    }
+
+    /**
+     * Define the props that are shared by default.
+     *
+     * @return array<string, mixed>
+     */
+    public function share(Request $request): array
+    {
+
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => $request->user() ? [
+                    //'id' => $this->id,
+                    'name' => $request->user()->name,
+                    'email' => $request->user()->email,
+                    'phone' => $request->user()->phone,
+                    'role' => $request->user()->getRoleNames()->first(),
+                ] : null,
+            ],
+            'ziggy' => function () use ($request) {
+                return array_merge((new Ziggy)->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
+            'categories' => fn () => CategoryResource::collection(Category::whereNull('category_id')->get()),
+            'flash' => [
+                'success' => fn () => $request->session()->get('success'),
+            ],
+        ]);
+    }
+}
