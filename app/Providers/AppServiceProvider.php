c1535fa3c132d9b07664528470600b8612dae2db
Leidy Estefania Poma Casas <leidypoma068@gmail.com>
2026-09-14 20:42:31 -0400

    chore: incorporar proveedores base de Laravel

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        JsonResource::withoutWrapping();
+        Blade::directive('money', function ($money) {
+            return "<?php echo '$ ' . number_format($money, 2); ?>";
+        });
+
+        Blade::directive('numberFormat', function ($number) {
+            return "<?php echo number_format($number, 2); ?>";
+        });
+    }
+}
